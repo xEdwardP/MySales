f92f1e4a063d51074cbc44f793f9f162fa7c5cc4
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2025-03-08 16:25:53 -0600

    Configure NiceAdmin template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::get('/home', [DashboardController::class, 'index'])->name('home');
